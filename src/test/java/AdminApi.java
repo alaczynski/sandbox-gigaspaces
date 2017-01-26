b13f8399911a3a5de70b6c8965c3b587ea9cc911
Adam Laczynski <adam.laczynski@gmail.com>
2017-01-26 23:58:12 +0100

    deployment

--- src/test/java/AdminApi.java
+++ src/test/java/AdminApi.java
@@ -4,7 +4,11 @@ import org.openspaces.admin.Admin;
 import org.openspaces.admin.AdminFactory;
 import org.openspaces.admin.alert.config.AlertConfiguration;
 import org.openspaces.admin.alert.config.CpuUtilizationAlertConfiguration;
+import org.openspaces.admin.application.ApplicationDeployment;
+import org.openspaces.admin.gsm.GridServiceManager;
+import org.openspaces.admin.pu.ProcessingUnitDeployment;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,5 +26,27 @@ public class AdminApi {
                 "high-threshold-perc", "80"));
         configurations.add(configuration);
         admin.getAlertManager().configure(configurations.toArray(new AlertConfiguration[configurations.size()]));
+    }
+
+    public void deployment() {
+        Admin admin = new AdminFactory().create();
+        GridServiceManager gridServiceManager = admin.getGridServiceManagers().waitForAtLeastOne();
+
+        // pu
+        ProcessingUnitDeployment processingUnitDeployment = new ProcessingUnitDeployment(new File("path.jar"));
+        processingUnitDeployment.name("pu-name");
+        gridServiceManager.deploy(processingUnitDeployment);
+
+        // app
+        ApplicationDeployment applicationDeployment = new ApplicationDeployment("app-name");
+        applicationDeployment.addProcessingUnitDeployment(new ProcessingUnitDeployment(new File("pu-1.jar")));
+        applicationDeployment.addProcessingUnitDeployment(new ProcessingUnitDeployment(new File("pu-2.jar")));
+        gridServiceManager.deploy(applicationDeployment);
+
+        // undeploy pu
+        admin.getProcessingUnits().waitFor("pu-name").undeployAndWait();
+
+        // undeploy app
+        admin.getApplications().waitFor("app-name").undeployAndWait();
     }
 }
